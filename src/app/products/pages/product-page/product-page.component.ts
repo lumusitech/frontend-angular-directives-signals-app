9d8f308b79660fc2f280195884feeeb8f7ae1d4d
lumusitech <lumusitech@gmail.com>
2023-10-28 22:41:03 -0300

    feat: add base product page component

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.css'],
 })
 export class ProductPageComponent {
+  private readonly fb = inject(FormBuilder);
 
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.email],
+    ],
+  });
+
+  public color: string = 'green';
+
+  // constructor(private readonly fb: FormBuilder) {}
+
+  changeColor() {
+    this.color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+  }
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProductPageComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
 })
 export class ProductsModule {}
