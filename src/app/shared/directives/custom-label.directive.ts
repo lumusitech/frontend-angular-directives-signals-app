7ac1f5d3f938ca5a3e67e3ec7451c67f03445cf3
lumusitech <lumusitech@gmail.com>
2023-10-30 08:21:23 -0300

    feat: add custom directive for manage errors

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,17 +1,64 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
 })
 export class CustomLabelDirective implements OnInit {
+  private _color: string = 'red';
   private htmlElement?: ElementRef<HTMLElement>;
+  private _errors?: ValidationErrors | null;
+
+  @Input() set color(color: string) {
+    this._color = color;
+    this.setStyle();
+  }
+
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+    console.log(value);
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = this.el;
-    this.htmlElement.nativeElement.innerHTML = 'Hello world';
   }
 
   ngOnInit(): void {
     console.log('custom directive active');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if (this.htmlElement)
+      this.htmlElement.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage() {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement!.nativeElement.innerHTML = 'No errors!';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    console.log(errors);
+
+    if (errors.includes('required')) {
+      this.htmlElement!.nativeElement.innerHTML = 'This field is required';
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlElement!.nativeElement.innerHTML = 'This field must be an email';
+      return;
+    }
+
+    if (errors.includes('minlength')) {
+      const current = this._errors!['minlength']['actualLength'];
+      const min = this._errors!['minlength']['requiredLength'];
+      this.htmlElement!.nativeElement.innerHTML = `This field must be at least ${min} characters, but now is ${current}`;
+      return;
+    }
   }
 }
