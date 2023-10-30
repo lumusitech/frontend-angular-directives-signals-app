031815589e940619c70ad17e051476e3704f2272
lumusitech <lumusitech@gmail.com>
2023-10-30 09:08:09 -0300

    feat: add basic menu

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css'],
+})
+export class SideMenuComponent {
+  public menuItems: MenuItem[] = [
+    { title: 'counter', route: 'counter' },
+    { title: 'user info', route: 'user-info' },
+    { title: 'mutations', route: 'properties' },
+  ];
+}
--- src/app/signals/signals.module.ts
+++ src/app/signals/signals.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { SignalsRoutingModule } from './signals-routing.module';
-import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
 import { CounterPageComponent } from './components/counter-page/counter-page.component';
-import { UserInfoPageComponent } from './components/user-info-page/user-info-page.component';
 import { PropertiesPageComponent } from './components/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { UserInfoPageComponent } from './components/user-info-page/user-info-page.component';
+import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
+import { SignalsRoutingModule } from './signals-routing.module';
 
 @NgModule({
   declarations: [
     SignalsLayoutComponent,
     CounterPageComponent,
     UserInfoPageComponent,
-    PropertiesPageComponent
+    PropertiesPageComponent,
+    SideMenuComponent,
   ],
   imports: [CommonModule, SignalsRoutingModule],
 })
